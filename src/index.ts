6294d2e91512a4ab269645390722328d632cddf5
Matthieu Hostache <matthieu.hostache@gmail.com>
2019-12-10 09:59:02 +0100

    Add missing Nav* exports

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export { Card } from './components/Card';
 export { Flex } from './components/Flex';
 export { Heading } from './components/Heading';
 export { Input } from './components/Input';
+export { Nav, NavItem, NavGroup } from './components/Nav';
 export { Table, Thead, Tbody, Tfoot, Tr, Th, Td } from './components/Table';
 export { Text } from './components/Text';
 
