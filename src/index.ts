33a0650e4bbc80b45f45df9793bb5612f5acb487
Matthieu Hostache <matthieu.hostache@gmail.com>
2019-12-10 09:47:55 +0100

    Add missing Input export

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { Button } from './components/Button';
 export { Card } from './components/Card';
 export { Flex } from './components/Flex';
 export { Heading } from './components/Heading';
+export { Input } from './components/Input';
 export { Table, Thead, Tbody, Tfoot, Tr, Th, Td } from './components/Table';
 export { Text } from './components/Text';
 
