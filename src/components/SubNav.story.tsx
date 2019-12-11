28e711c1d4712ef1306b9289cfd237974954fc4e
paulocfjunior <paulo@containo.us>
2019-12-11 14:55:13 -0300

    Add <SubNav> and <SubNavItem> components

---
+++ src/components/SubNav.story.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Box } from './Box';
+import { SubNav, SubNavItem } from './SubNav';
+import { Text } from './Text';
+
+function SubNavStory() {
+  return (
+    <>
+      <Box mb="4">
+        <SubNav>
+          <SubNavItem as="a" href="#">
+            This is a link
+          </SubNavItem>
+          <SubNavItem as="button" variant="active" onClick={() => {}}>
+            This is a button
+          </SubNavItem>
+          <SubNavItem>
+            <Text size={2} textColor="grays.5">Defaults to Button</Text>
+          </SubNavItem>
+        </SubNav>
+      </Box>
+    </>
+  );
+}
+
+storiesOf('Components|SubNav', module).add('default', () => <SubNavStory />);
