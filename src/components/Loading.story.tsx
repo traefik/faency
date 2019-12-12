dbf9e7b73dca9f979e368e03e82142ad7a448fac
paulocfjunior <paulo@containo.us>
2019-12-12 10:56:58 -0300

    Add <Loading> component

---
+++ src/components/Loading.story.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Loading } from './Loading';
+import { Box } from './Box';
+import { useState, useEffect } from '@storybook/addons';
+
+storiesOf('Components|Loading', module).add('default', () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const randomNumber = () => Math.round(Math.random() * 100);
+    const id = setTimeout(() => setProgress(randomNumber()), 1000);
+    return () => clearTimeout(id);
+  }, [progress, setProgress]);
+
+  return (
+    <>
+      <Box mb={1}>
+        Continuous:
+        <Loading />
+      </Box>
+      <Box>
+        Percentage ({progress}%):
+        <Loading progress={progress} />
+      </Box>
+    </>
+  )
+});
---
+++ src/components/Loading.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {css as _css, keyframes} from 'styled-components';
+import css from '@styled-system/css';
+import {Box as BoxPrimitive, BoxProps as BoxPrimitiveProps} from 'mdlz-prmtz';
+
+type LoadingProps = BoxPrimitiveProps & {
+  progress?: number;
+};
+
+const loadingAnimation = (props: LoadingProps) => keyframes`
+  50% {
+    width: 100%;
+    margin-left: 0;
+  }
+  100% {
+    width: 0%;
+    margin-left: 100%;
+  }
+`;
+
+export const Loading = React.forwardRef<HTMLDivElement, LoadingProps>((props, ref) => (
+  <BoxPrimitive {...props} ref={ref} css={[
+    css({
+      height: '4px',
+      '::after': {
+        content: `""`,
+        backgroundColor: 'primary',
+        display: 'flex',
+        height: '4px',
+        width: 0,
+      }
+    }),
+    props.hasOwnProperty('progress') ? css({
+      '::after': {
+        transition: 'width 1s ease-in-out',
+        width: `${props.progress}%`,
+      }
+    }) : _css`
+      &::after {
+        animation: ${loadingAnimation(props)} 2s ease-in-out infinite
+      }
+    `,
+  ]} />
+));
