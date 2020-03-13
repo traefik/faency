3dd416492e49d6e80d3754f91ac771381d62d6eb
Paulo Junior <paulo@containo.us>
2020-03-13 10:42:27 -0300

    Add tests to Avatar component

--- packages/faency/src/components/Avatar..test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { Avatar } from './Avatar'
-
-describe('Avatar component', () => {
-  test('renders without crashing', () => {
-    render(<Avatar src="https://avatars0.githubusercontent.com/u/invalid-user" />)
-  })
-})
