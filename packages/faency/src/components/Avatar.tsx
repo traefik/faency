631d035cbd4a45a989bdd35154a4614619c187d5
Paulo Junior <paulo@containo.us>
2020-03-13 10:27:30 -0300

    Make Avatar component behave properly according to the current Theme

--- packages/faency/src/components/Avatar.tsx
+++ packages/faency/src/components/Avatar.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, forwardRef } from 'react'
-import { Avatar as AvatarPrimitive, AvatarProps as AvatarPrimitiveProps } from 'mdlz-prmtz'
+import { Avatar as AvatarPrimitive, AvatarProps as AvatarPrimitiveProps } from '@modulz/primitives'
 import { ThemeContext } from 'styled-components'
+import useDarkMode from 'use-dark-mode'
 
 type AvatarProps = AvatarPrimitiveProps & {
   textColor?: string
@@ -9,10 +10,28 @@ type AvatarProps = AvatarPrimitiveProps & {
 
 export const Avatar = forwardRef<HTMLSpanElement, AvatarProps>((props, forwardedRef) => {
   const theme = useContext(ThemeContext)
+  const darkMode = useDarkMode(false)
+  let forwardedProps = props
+
+  if (typeof props.variant === 'undefined') {
+    forwardedProps = { ...props, variant: darkMode.value ? 'dark' : 'light' }
+  }
+
+  // FIXME: Needed to handle the colors relative to the theme, since the
+  // Black and White colors are inverted on Dark theme. Should be solved on faency#75.
+  const relativeColors = darkMode.value
+    ? {
+        black: theme.colors.white,
+        white: theme.colors.black,
+      }
+    : {
+        black: theme.colors.black,
+        white: theme.colors.white,
+      }
 
   return (
     <AvatarPrimitive
-      {...props}
+      {...forwardedProps}
       ref={forwardedRef}
       styleConfig={{
         base: {
@@ -24,7 +43,7 @@ export const Avatar = forwardRef<HTMLSpanElement, AvatarProps>((props, forwarded
               width: theme.space[8],
               fontSize: theme.sizes[3],
               fontWeight: 'bold',
-              color: theme.colors.white,
+              color: relativeColors.white,
               transition: 'all 200ms ease-in-out',
             },
           },
@@ -36,7 +55,7 @@ export const Avatar = forwardRef<HTMLSpanElement, AvatarProps>((props, forwarded
                 normal: {
                   '&:hover': {
                     boxShadow: `0 0 0 2px ${theme.colors.blue}`,
-                    backgroundColor: theme.colors.white,
+                    backgroundColor: relativeColors.white,
                     color: `${theme.colors.blue}`,
                     '*': {
                       color: `${theme.colors.blue}`,
@@ -48,8 +67,12 @@ export const Avatar = forwardRef<HTMLSpanElement, AvatarProps>((props, forwarded
             dark: {
               avatar: {
                 normal: {
+                  color: relativeColors.white,
+                  '*': {
+                    color: `${relativeColors.white} !important`,
+                  },
                   '&:hover': {
-                    boxShadow: `0 0 0 2px ${theme.colors.white}`,
+                    boxShadow: `0 0 0 2px ${relativeColors.white}`,
                   },
                 },
               },
@@ -62,5 +85,3 @@ export const Avatar = forwardRef<HTMLSpanElement, AvatarProps>((props, forwarded
 })
 
 Avatar.displayName = 'Avatar'
-
-Avatar.defaultProps = { variant: 'light' }
