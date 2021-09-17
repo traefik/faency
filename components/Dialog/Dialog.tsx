ae36b82c616fa1cbf594fdfc47970fb7a28dcfe6
Matthieu Hostache <matthieu.hostache@gmail.com>
2021-09-17 15:10:09 +0200

    fix: build issue

--- components/Dialog/Dialog.tsx
+++ components/Dialog/Dialog.tsx
@@ -74,7 +74,7 @@ export const DialogContent = React.forwardRef<
   <StyledContent {...props} ref={forwardedRef}>
     {children}
     <StyledCloseButton asChild>
-      <IconButton variant="ghost">
+      <IconButton ghost>
         <Cross1Icon />
       </IconButton>
     </StyledCloseButton>
