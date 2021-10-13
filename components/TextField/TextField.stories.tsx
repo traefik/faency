2155680bfda97d00e72b25f2691cbfd3c30adffa
Seedy <seedy.dupuis@gmail.com>
2021-10-13 17:31:49 +0200

    TextField: stories for search, invalid, clearable, compounds

--- components/TextField/TextField.stories.tsx
+++ components/TextField/TextField.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { TextField, TextFieldProps, TextFieldVariants } from './TextField';
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import { modifyVariantsForStory } from '../../utils/modifyVariantsForStory';
 
 const BaseTextField = (props: TextFieldProps): JSX.Element => <TextField {...props} />;
@@ -35,6 +36,22 @@ PasswordType.args = { type: 'password', value: 'value' };
 export const Invalid = Template.bind({});
 
 Invalid.args = { state: 'invalid' };
+
+export const Clearable = Template.bind({});
+
+Clearable.args = { clearable: true };
+
+export const ClearableInvalid = Template.bind({});
+
+ClearableInvalid.args = { clearable: true, state: 'invalid' };
+
+export const Search = Template.bind({});
+
+Search.args = { startAdornment: <MagnifyingGlassIcon />, placeholder: 'Search...' }
+
+export const SearchClearable = Template.bind({});
+
+SearchClearable.args = { startAdornment: <MagnifyingGlassIcon />, placeholder: 'Search...', clearable: true };
 
 export const Disabled = Template.bind({});
 
