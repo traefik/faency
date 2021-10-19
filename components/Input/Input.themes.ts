1e85d3950a58f9244ab5c05ced5ef84c18075bb6
Cedric Dupuis <cedric.dupuis@traefik.io>
2021-10-19 16:50:48 +0200

    fix: Input theme, rework opacity computations

--- components/Input/Input.themes.ts
+++ components/Input/Input.themes.ts
@@ -23,29 +23,29 @@ export namespace Theme {
     inputFocusBorder: 'hsl(68, 53%, 48%)',
     inputFocusBorderGhost: 'hsl(68, 53%, 56%)',
     inputHoverBg: 'hsla(68, 80%, 94%, 0.33)',
-    inputHoverBorderGhost: '$deepBlue4',
+    inputHoverBorderGhost: 'hsla(208, 9%, 73%, 0.9)', // inputBorder with opacity
     inputText: 'hsla(0, 0%, 0%, 0.74)',
     inputTextFocused: 'hsl(0, 0%, 0%)',
     inputPlaceholder: 'hsla(0, 0%, 0%, 0.5)',
     inputDisabledText: 'hsla(0, 0%, 0%, 0.35)',
-    inputDisabledBorder: '$deepBlue3',
+    inputDisabledBorder: 'hsla(208, 9%, 73%, 0.7)', // inputBorder with opacity
     inputInvalidBorder: '$red8',
   });
 
   export const getDark: Factory = (primaryColor) => ({
     wrapperBg: 'hsl(209, 38%, 12%)',
     inputBg: 'hsla(0, 0%, 100%, 0)',
-    inputBorder: '$deepBlue7',
+    inputBorder: '$deepBlue6',
     inputFocusBg: 'hsla(0, 0%, 0%, 0.15)',
     inputFocusBorder: 'hsl(67, 78%, 84%)',
     inputFocusBorderGhost: 'hsl(67, 78%, 92%)',
     inputHoverBg: 'hsla(68, 81%, 80%, 0.1)',
-    inputHoverBorderGhost: '$deepBlue6',
+    inputHoverBorderGhost: 'hsla(208, 11%, 45%, 0.9)', // inputBorder with opacity
     inputText: 'hsla(0, 0%, 100%, 0.8)',
     inputTextFocused: 'hsl(0, 0%, 100%)',
     inputPlaceholder: 'hsla(0, 0%, 100%, 0.51)',
     inputDisabledText: 'hsla(0, 0%, 100%, 0.35)',
-    inputDisabledBorder: '$deepBlue5',
+    inputDisabledBorder: 'hsla(208, 11%, 45%, 0.7)', // inputBorder with opacity
     inputInvalidBorder: '$red8',
   });
 }
