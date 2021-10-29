0759136813a0a4ba9e0bf168acc390fa78b12270
Paulo Junior <paulo.cezar@traefik.io>
2021-10-28 21:53:14 -0300

    feedback: simulate navigation when clicking on menu items

--- components/Navigation/Navigation.stories.tsx
+++ components/Navigation/Navigation.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import {
   DashboardIcon,
@@ -6,8 +6,6 @@ import {
   GearIcon,
   QuestionMarkCircledIcon,
 } from '@radix-ui/react-icons';
-import { VariantProps } from '@stitches/react';
-import { CSS } from '../../stitches.config';
 
 import {
   NavigationDrawer,
@@ -15,19 +13,22 @@ import {
   NavigationLink,
   NavigationButton,
   NavigationDrawerProps,
-  NavigationContainerProps,
   NavigationDrawerVariants,
   NavigationItem,
 } from './Navigation';
 import { modifyVariantsForStory } from '../../utils/modifyVariantsForStory';
 import { Badge } from '../Badge';
 import { Text } from '../Text';
+import { useDebouncedCallback } from 'use-debounce/lib';
 
 export default {
   title: 'Components/Navigation',
   component: NavigationDrawer,
-  subComponents: { NavigationContainer, NavigationLink, NavigationButton },
-  // argTypes: { onClick: { action: 'clicked' } },
+  argTypes: {
+    elevation: {
+      control: 'number',
+    },
+  },
 } as ComponentMeta<typeof NavigationDrawer>;
 
 const BaseNavigationDrawer = (props: NavigationDrawerProps): JSX.Element => (
@@ -38,14 +39,29 @@ const NavigationDrawerForStory = modifyVariantsForStory<
   NavigationDrawerProps
 >(BaseNavigationDrawer);
 
-const Template: ComponentStory<typeof NavigationDrawerForStory> = (args) => (
-  <NavigationDrawerForStory {...args}>
-    <NavigationButton active>Dashboard</NavigationButton>
-    <NavigationButton>Profile</NavigationButton>
-    <NavigationButton>Settings</NavigationButton>
-    <NavigationLink href="#">Help</NavigationLink>
-  </NavigationDrawerForStory>
-);
+const useNavigationSample = () => {
+  const [currentRoute, setCurrentRoute] = useState('/');
+  const navigateTo = useDebouncedCallback((route: string) => setCurrentRoute(route), 300);
+  const navigationHandlerProps = (route: string) => ({
+    active: route === currentRoute,
+    onClick: () => navigateTo(route),
+  });
+
+  return { navigationHandlerProps };
+};
+
+const Template: ComponentStory<typeof NavigationDrawerForStory> = (args) => {
+  const { navigationHandlerProps } = useNavigationSample();
+
+  return (
+    <NavigationDrawerForStory {...args}>
+      <NavigationButton {...navigationHandlerProps('/')}>Dashboard</NavigationButton>
+      <NavigationButton {...navigationHandlerProps('/profile')}>Profile</NavigationButton>
+      <NavigationButton {...navigationHandlerProps('/settings')}>Settings</NavigationButton>
+      <NavigationButton {...navigationHandlerProps('/help')}>Help</NavigationButton>
+    </NavigationDrawerForStory>
+  );
+};
 
 export const Basic = Template.bind({});
 
@@ -53,36 +69,45 @@ Basic.args = {
   elevation: 1,
 };
 
-const AdornmentsTemplate: ComponentStory<typeof NavigationDrawerForStory> = (args) => (
-  <NavigationDrawer css={{ height: '100%' }} {...args}>
-    <NavigationItem startAdornment={<DashboardIcon />} active>
-      Dashboard
-    </NavigationItem>
-    <NavigationItem startAdornment={<PersonIcon />}>Profile</NavigationItem>
-    <NavigationItem
-      as="button"
-      startAdornment={<GearIcon />}
-      endAdornment={
-        <Badge
-          css={{
-            height: '$3',
-            borderRadius: '$pill',
-            fontSize: '$0',
-            fontWeight: 500,
-            color: '$hiContrast',
-          }}
-        >
-          99+
-        </Badge>
-      }
-    >
-      Settings
-    </NavigationItem>
-    <NavigationItem startAdornment={<QuestionMarkCircledIcon />} as="a" href="#">
-      Help
-    </NavigationItem>
-  </NavigationDrawer>
-);
+const AdornmentsTemplate: ComponentStory<typeof NavigationDrawerForStory> = (args) => {
+  const { navigationHandlerProps } = useNavigationSample();
+
+  return (
+    <NavigationDrawer css={{ height: '100%' }} {...args}>
+      <NavigationItem {...navigationHandlerProps('/')} startAdornment={<DashboardIcon />}>
+        Dashboard
+      </NavigationItem>
+      <NavigationItem {...navigationHandlerProps('/profile')} startAdornment={<PersonIcon />}>
+        Profile
+      </NavigationItem>
+      <NavigationItem
+        {...navigationHandlerProps('/settings')}
+        startAdornment={<GearIcon />}
+        endAdornment={
+          <Badge
+            css={{
+              height: '$3',
+              borderRadius: '$pill',
+              fontSize: '$0',
+              fontWeight: 500,
+              color: '$hiContrast',
+            }}
+          >
+            99+
+          </Badge>
+        }
+      >
+        Settings
+      </NavigationItem>
+      <NavigationItem
+        {...navigationHandlerProps('/help')}
+        startAdornment={<QuestionMarkCircledIcon />}
+      >
+        Help
+      </NavigationItem>
+    </NavigationDrawer>
+  );
+};
 
 export const Adornments = AdornmentsTemplate.bind({});
 
@@ -90,33 +115,44 @@ Adornments.args = {
   elevation: 1,
 };
 
-const MultipleSectionsTemplate: ComponentStory<typeof NavigationDrawerForStory> = (args) => (
-  <NavigationDrawer css={{ height: '90vh' }} {...args}>
-    <NavigationContainer
-      css={{
-        mb: '$3',
-        p: '$4',
-        textAlign: 'center',
-        border: '1px solid hsla(0, 0%, 50%, 0.51)',
-        borderRadius: '$3',
-      }}
-    >
-      <Text>Company Logo</Text>
-    </NavigationContainer>
-    <NavigationContainer css={{ flexGrow: 1 }}>
-      <NavigationItem startAdornment={<DashboardIcon />} active>
-        Dashboard
-      </NavigationItem>
-      <NavigationItem startAdornment={<PersonIcon />}>Profile</NavigationItem>
-      <NavigationItem startAdornment={<GearIcon />}>Settings</NavigationItem>
-    </NavigationContainer>
-    <NavigationContainer>
-      <NavigationItem startAdornment={<QuestionMarkCircledIcon />} as="a" href="#">
-        Help
-      </NavigationItem>
-    </NavigationContainer>
-  </NavigationDrawer>
-);
+const MultipleSectionsTemplate: ComponentStory<typeof NavigationDrawerForStory> = (args) => {
+  const { navigationHandlerProps } = useNavigationSample();
+
+  return (
+    <NavigationDrawer css={{ height: '90vh' }} {...args}>
+      <NavigationContainer
+        css={{
+          mb: '$3',
+          p: '$4',
+          textAlign: 'center',
+          border: '1px solid hsla(0, 0%, 50%, 0.51)',
+          borderRadius: '$3',
+        }}
+      >
+        <Text>Company Logo</Text>
+      </NavigationContainer>
+      <NavigationContainer css={{ flexGrow: 1 }}>
+        <NavigationItem {...navigationHandlerProps('/')} startAdornment={<DashboardIcon />}>
+          Dashboard
+        </NavigationItem>
+        <NavigationItem {...navigationHandlerProps('/profile')} startAdornment={<PersonIcon />}>
+          Profile
+        </NavigationItem>
+        <NavigationItem {...navigationHandlerProps('/settings')} startAdornment={<GearIcon />}>
+          Settings
+        </NavigationItem>
+      </NavigationContainer>
+      <NavigationContainer>
+        <NavigationItem
+          {...navigationHandlerProps('/help')}
+          startAdornment={<QuestionMarkCircledIcon />}
+        >
+          Help
+        </NavigationItem>
+      </NavigationContainer>
+    </NavigationDrawer>
+  );
+};
 
 export const MultiSection = MultipleSectionsTemplate.bind({});
 
