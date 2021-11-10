3d1755c46475002c172d29f323d8ea73477f4b54
Paulo Junior <paulo.cezar@traefik.io>
2021-11-10 04:56:21 -0300

    proposal: set background color based on elevation

--- components/Elevation/Elevation.tsx
+++ components/Elevation/Elevation.tsx
@@ -7,22 +7,27 @@ export const elevationVariant: ElevationVariant = {
     boxShadow: 'none',
   },
   1: {
+    bc: '$01dp',
     boxShadow:
       '0 1px 5px 0 hsla(0, 0%, 0%, 0.2), 0 3px 1px -2px hsla(0, 0%, 0%, 0.12), 0 2px 2px 0 hsla(0, 0%, 0%, 0.14)',
   },
   2: {
+    bc: '$02dp',
     boxShadow:
       '0 2px 4px -1px hsla(0, 0%, 0%, 0.2), 0 1px 10px 0 hsla(0, 0%, 0%, 0.12), 0 4px 5px 0 hsla(0, 0%, 0%, 0.14)',
   },
   3: {
+    bc: '$03dp',
     boxShadow:
       '0 3px 5px -1px hsla(0, 0%, 0%, 0.2), 0 1px 18px 0 hsla(0, 0%, 0%, 0.12), 0 6px 10px 0 hsla(0, 0%, 0%, 0.14)',
   },
   4: {
+    bc: '$04dp',
     boxShadow:
       '0 7px 8px -4px hsla(0, 0%, 0%, 0.2), 0 5px 22px 4px hsla(0, 0%, 0%, 0.12), 0 12px 17px 2px hsla(0, 0%, 0%, 0.14)',
   },
   5: {
+    bc: '$05dp',
     boxShadow:
       '0 11px 15px -7px hsla(0, 0%, 0%, 0.2), 0 9px 46px 8px hsla(0, 0%, 0%, 0.12), 0 24px 38px 3px hsla(0, 0%, 0%, 0.14)',
   },
