ae33378960c865a077e0d830a77d7aec01f497ef
Cedric Dupuis <cedric.dupuis@traefik.io>
2021-12-02 12:12:41 +0100

    fix: overlayStyles, wrong hsla computation

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,7 +1,7 @@
 import { styled, CSS } from '../stitches.config';
 
 export const overlayStyles: CSS = {
-  backgroundColor: 'hsla(0, 0, 0, 0.7)',
+  backgroundColor: 'hsla(0, 0%, 0%, 0.7)',
 };
 
 export const Overlay = styled('div', overlayStyles);
