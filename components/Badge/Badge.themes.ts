e1c282fcb8852fcc44282cb5dbb493d90e58775f
Cedric Dupuis <cedric.dupuis@traefik.io>
2021-12-06 16:53:32 +0100

    refactor: badge theme, remove obsolete theme variables

--- components/Badge/Badge.themes.ts
+++ components/Badge/Badge.themes.ts
@@ -5,29 +5,16 @@ import { ColorInfo } from '../../utils/getPrimaryColorInfo';
 
 export namespace Theme {
   type Colors = {
-    badgeDefaultBackground: Property.Color;
-    badgeDefaultText: Property.Color;
-    badgeNeonBackground: Property.Color;
-    badgeNeonText: Property.Color;
     badgeInteractiveBackground: Property.Color;
   };
 
   type Factory = (primaryColor: ColorInfo) => Colors;
 
   export const getLight: Factory = () => ({
-    badgeDefaultBackground: '$slate7',
-    badgeDefaultText: '$slate10',
-    badgeNeonBackground: tinycolor(colors.neon.neon9).setAlpha(0.2).toHslString(),
-    badgeNeonText: '$neon9',
-    badgeInteractiveBackground: 'rgba(0,0,0,.05)',
+    badgeInteractiveBackground: tinycolor('black').setAlpha(0.05).toHslString(),
   });
 
   export const getDark: Factory = () => ({
-    badgeDefaultBackground: '$slate5',
-    badgeDefaultText: '$slate10',
-    badgePrimaryText: '$primary',
-    badgeNeonBackground: '$neon11',
-    badgeNeonText: '$neon7',
     badgeInteractiveBackground: tinycolor('black').setAlpha(0.1).toHslString(),
   });
 }
