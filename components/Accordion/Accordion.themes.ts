b72f8915e899a0d64c738f7899ccb7fa0a100d7b
Cedric Dupuis <cedric.dupuis@traefik.io>
2022-01-18 22:39:57 +0100

    feat: Accordion, update styles, add size variants, stories

--- components/Accordion/Accordion.themes.ts
+++ components/Accordion/Accordion.themes.ts
@@ -7,6 +7,8 @@ export namespace Theme {
     accordionBackground: Property.Color;
     accordionHoverShadow: Property.Color;
     accordionActiveShadow: Property.Color;
+    accordionText: Property.Color;
+    accordionLabel: Property.Color;
   };
 
   type Factory = (primaryColor: ColorInfo) => Colors;
@@ -15,11 +17,15 @@ export namespace Theme {
     accordionBackground: 'white',
     accordionHoverShadow: tinycolor('black').setAlpha(0.05).toHslString(),
     accordionActiveShadow: tinycolor('black').setAlpha(0.2).toHslString(),
+    accordionText: tinycolor('black').setAlpha(0.74).toHslString(),
+    accordionLabel: tinycolor('black').setAlpha(0.51).toHslString(),
   });
 
   export const getDark: Factory = () => ({
     accordionBackground: '$deepBlue2',
     accordionHoverShadow: tinycolor('white').setAlpha(0.05).toHslString(),
     accordionActiveShadow: tinycolor('white').setAlpha(0.2).toHslString(),
+    accordionText: tinycolor('white').setAlpha(0.74).toHslString(),
+    accordionLabel: tinycolor('white').setAlpha(0.51).toHslString(),
   });
 }
