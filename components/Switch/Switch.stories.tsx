1d90cfaa67a154dc9c5a33540b2e68f7ad610ac1
Cedric Dupuis <cedric.dupuis@traefik.io>
2022-01-28 11:01:00 +0100

    feat: Switch, add label examples

--- components/Switch/Switch.stories.tsx
+++ components/Switch/Switch.stories.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Switch, SwitchProps, SwitchVariants } from './Switch';
+import { Flex } from '../Flex';
+import { Label } from '../Label';
 import { modifyVariantsForStory } from '../../utils/modifyVariantsForStory';
+import ignoreArgType from '../../utils/ignoreArgType';
 
 const BaseSwitch = (props: SwitchProps): JSX.Element => <Switch {...props} />;
 const SwitchForStory = modifyVariantsForStory<
@@ -29,3 +32,85 @@ Large.args = { size: '2' };
 export const Disabled = Template.bind({});
 
 Disabled.args = { disabled: true };
+
+export const Labelled: ComponentStory<typeof SwitchForStory> = ({ id, ...args }) => (
+  <>
+    <Flex align="center">
+      <Label variant="contrast" htmlFor={id}>label</Label>
+      <SwitchForStory id={id} {...args} />
+    </Flex>
+  </>
+);
+Labelled.args = {
+  id: 'labelled',
+};
+ignoreArgType('id', Labelled);
+
+interface ExtendedSwitchProps extends React.ComponentProps<typeof SwitchForStory> {
+  invalid?: boolean
+}
+
+export const LabelAndTitle: ComponentStory<(props: ExtendedSwitchProps) => JSX.Element> = ({ id, onFocus, onBlur, invalid, disabled, ...args }) => {
+  const [hasFocus, setHasFocus] = React.useState(false);
+
+  const titleVariant = React.useMemo(() => {
+    if (disabled) {
+      return 'subtle'
+    }
+    if (invalid) {
+      return 'invalid'
+    }
+    if (hasFocus) {
+      return 'contrast'
+    }
+    return 'default'
+  }, [invalid, disabled, hasFocus])
+
+  const labelVariant = React.useMemo(() => {
+    if (invalid) {
+      return 'invalid'
+    }
+    if (disabled) {
+      return 'subtle'
+    }
+    return 'contrast'
+  }, [invalid, disabled]
+  );
+
+
+  const handleFocus = React.useCallback(
+    (e) => {
+      if (onFocus) {
+        onFocus(e)
+      }
+      setHasFocus(true)
+    },
+    [onFocus, setHasFocus],
+  )
+
+  const handleBlur = React.useCallback(
+    (e) => {
+      if (onBlur) {
+        onBlur(e)
+      }
+      setHasFocus(false)
+    },
+    [onBlur, setHasFocus],
+  )
+
+  return (
+    <>
+      <Label htmlFor={id} variant={titleVariant}>title</Label>
+      <Flex align="center">
+        <Label variant={labelVariant} htmlFor={id}>label</Label>
+        <SwitchForStory id={id} disabled={disabled} onBlur={handleBlur} onFocus={handleFocus} {...args} />
+      </Flex>
+    </>
+  );
+}
+LabelAndTitle.args = {
+  id: 'label-title',
+  invalid: false,
+  disabled: false,
+}
+ignoreArgType('id', LabelAndTitle);
