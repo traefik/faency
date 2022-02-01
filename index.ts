b4b18a536b18333073c53c78c5e5f1203a102e71
Cedric Dupuis <cedric.dupuis@traefik.io>
2022-02-01 16:34:00 +0100

    chore: export components from AriaTable

--- index.ts
+++ index.ts
@@ -45,6 +45,7 @@ export { Radio, RadioGroup } from './components/Radio';
 export { Select } from './components/Select';
 export { Skeleton } from './components/Skeleton';
 export { Switch } from './components/Switch';
+export { Caption as AriaCaption, Tbody as AriaTbody, Tfoot as AriaTfoot, Tr as AriaTr, Th as AriaTh, Td as AriaTd, Thead as AriaThead, Table as AriaTable } from './components/AriaTable'
 export { Caption, Tbody, Tfoot, Tr, Th, Td, Thead, Table } from './components/Table';
 export { Text } from './components/Text';
 export { Tooltip } from './components/Tooltip';
