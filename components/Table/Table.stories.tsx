0de3d83021837d1fb02ddd497e7387f5f3b7885d
Seedy <cedric.dupuis@traefik.io>
2022-02-03 17:24:09 +0100

    feat: visuallyHidden + table accessible examples

--- components/Table/Table.stories.tsx
+++ components/Table/Table.stories.tsx
@@ -1,9 +1,12 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
-import { Table, TableProps, TableVariants, Tbody, Td, Th, Thead, Tr } from './Table';
+import { Table, TableProps, TableVariants, Tbody, Td, Th, Thead, Tr, Tfoot, Caption } from './Table';
 import { Badge } from '../Badge';
 import { Card } from '../Card';
+import { Flex } from '../Flex';
+import { Heading } from '../Heading';
 import { modifyVariantsForStory } from '../../utils/modifyVariantsForStory';
+import { VisuallyHidden } from '../VisuallyHidden';
 
 const BaseTable = (props: TableProps): JSX.Element => <Table {...props} />;
 const TableForStory = modifyVariantsForStory<TableVariants, TableProps>(BaseTable);
@@ -190,3 +193,215 @@ export const Interactive: ComponentStory<any> = (args) => (
 Interactive.args = {
   interactive: true,
 };
+
+
+export const WithFooter: ComponentStory<any> = (args) => (
+  <Card>
+    <TableForStory {...args}>
+      <Thead>
+        <Th>Firstname</Th>
+        <Th>Lastname</Th>
+        <Th>Status</Th>
+        <Th>Role</Th>
+      </Thead>
+      <Tbody>
+        <Tr>
+          <Td>John</Td>
+          <Td>Doe</Td>
+          <Td>
+            <Badge variant="green">Connected</Badge>
+          </Td>
+          <Td>Developer</Td>
+        </Tr>
+        <Tr>
+          <Td>Johny</Td>
+          <Td>Depp</Td>
+          <Td>
+            <Badge variant="orange">AFK</Badge>
+          </Td>
+          <Td>Actor</Td>
+        </Tr>
+        <Tr>
+          <Td>Natalie</Td>
+          <Td>Portman</Td>
+          <Td>
+            <Badge variant="green">Connected</Badge>
+          </Td>
+          <Td>Actor</Td>
+        </Tr>
+        <Tr>
+          <Td>Luke</Td>
+          <Td>Skywalker</Td>
+          <Td>
+            <Badge variant="red">Disconnected</Badge>
+          </Td>
+          <Td>Star wars</Td>
+        </Tr>
+      </Tbody>
+      <Tfoot>
+        <Tr>
+          <Td colSpan={4}>
+            Footer information
+          </Td>
+        </Tr>
+      </Tfoot>
+    </TableForStory>
+  </Card>
+)
+
+export const WithCaption: ComponentStory<any> = (args) => {
+  const id = "described-heading"
+  const title = 'Title not child of table'
+  return (
+    <>
+      <Flex direction="column" gap="4">
+        <Card>
+          <TableForStory {...args}>
+            <Caption>Caption child of table</Caption>
+            <Thead>
+              <Th>Firstname</Th>
+              <Th>Lastname</Th>
+              <Th>Status</Th>
+              <Th>Role</Th>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>John</Td>
+                <Td>Doe</Td>
+                <Td>
+                  <Badge variant="green">Connected</Badge>
+                </Td>
+                <Td>Developer</Td>
+              </Tr>
+              <Tr>
+                <Td>Johny</Td>
+                <Td>Depp</Td>
+                <Td>
+                  <Badge variant="orange">AFK</Badge>
+                </Td>
+                <Td>Actor</Td>
+              </Tr>
+              <Tr>
+                <Td>Natalie</Td>
+                <Td>Portman</Td>
+                <Td>
+                  <Badge variant="green">Connected</Badge>
+                </Td>
+                <Td>Actor</Td>
+              </Tr>
+              <Tr>
+                <Td>Luke</Td>
+                <Td>Skywalker</Td>
+                <Td>
+                  <Badge variant="red">Disconnected</Badge>
+                </Td>
+                <Td>Star wars</Td>
+              </Tr>
+            </Tbody>
+          </TableForStory>
+        </Card>
+        <div>
+          <Heading size="4" as="h1">
+            {title}
+          </Heading>
+          <Card>
+            <TableForStory {...args}>
+              <VisuallyHidden asChild>
+                <Caption>{title}</Caption>
+              </VisuallyHidden>
+              <Thead>
+                <Th>Firstname</Th>
+                <Th>Lastname</Th>
+                <Th>Status</Th>
+                <Th>Role</Th>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>John</Td>
+                  <Td>Doe</Td>
+                  <Td>
+                    <Badge variant="green">Connected</Badge>
+                  </Td>
+                  <Td>Developer</Td>
+                </Tr>
+                <Tr>
+                  <Td>Johny</Td>
+                  <Td>Depp</Td>
+                  <Td>
+                    <Badge variant="orange">AFK</Badge>
+                  </Td>
+                  <Td>Actor</Td>
+                </Tr>
+                <Tr>
+                  <Td>Natalie</Td>
+                  <Td>Portman</Td>
+                  <Td>
+                    <Badge variant="green">Connected</Badge>
+                  </Td>
+                  <Td>Actor</Td>
+                </Tr>
+                <Tr>
+                  <Td>Luke</Td>
+                  <Td>Skywalker</Td>
+                  <Td>
+                    <Badge variant="red">Disconnected</Badge>
+                  </Td>
+                  <Td>Star wars</Td>
+                </Tr>
+              </Tbody>
+            </TableForStory>
+          </Card>
+        </div>
+        <div>
+          <Heading id={id} size="4" as="h1">
+            {title}
+          </Heading>
+          <Card>
+            <TableForStory aria-describedby={id} {...args}>
+              <Thead>
+                <Th>Firstname</Th>
+                <Th>Lastname</Th>
+                <Th>Status</Th>
+                <Th>Role</Th>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>John</Td>
+                  <Td>Doe</Td>
+                  <Td>
+                    <Badge variant="green">Connected</Badge>
+                  </Td>
+                  <Td>Developer</Td>
+                </Tr>
+                <Tr>
+                  <Td>Johny</Td>
+                  <Td>Depp</Td>
+                  <Td>
+                    <Badge variant="orange">AFK</Badge>
+                  </Td>
+                  <Td>Actor</Td>
+                </Tr>
+                <Tr>
+                  <Td>Natalie</Td>
+                  <Td>Portman</Td>
+                  <Td>
+                    <Badge variant="green">Connected</Badge>
+                  </Td>
+                  <Td>Actor</Td>
+                </Tr>
+                <Tr>
+                  <Td>Luke</Td>
+                  <Td>Skywalker</Td>
+                  <Td>
+                    <Badge variant="red">Disconnected</Badge>
+                  </Td>
+                  <Td>Star wars</Td>
+                </Tr>
+              </Tbody>
+            </TableForStory>
+          </Card>
+        </div>
+      </Flex>
+    </>
+  )
+}
---
+++ components/VisuallyHidden/VisuallyHidden.stories.tsx
@@ -0,0 +1,59 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import { VisuallyHidden } from './VisuallyHidden';
+import { Table, Caption } from '../Table'
+import { Card } from '../Card';
+import { GearIcon } from '@radix-ui/react-icons';
+import { styled } from '../../stitches.config';
+
+const FlexButton = styled('button', {
+  m: 0,
+  p: 0,
+  color: '$hiContrast',
+  border: '1px dashed $hiContrast',
+  display: 'flex',
+  background: 'none'
+});
+
+const ContrastDiv = styled('div', {
+  color: '$hiContrast',
+});
+
+export default {
+  title: 'Components/VisuallyHidden',
+  component: VisuallyHidden,
+} as ComponentMeta<typeof VisuallyHidden>;
+
+export const Basic: ComponentStory<typeof VisuallyHidden> = (args) => (
+  <ContrastDiv>
+    <VisuallyHidden {...args}>Hidden visually</VisuallyHidden>
+  </ContrastDiv>
+);
+
+export const HiddenButtonText: ComponentStory<typeof VisuallyHidden> = (args) => (
+  <FlexButton>
+    <GearIcon />
+    <VisuallyHidden {...args}>Settings</VisuallyHidden>
+  </FlexButton>
+)
+
+export const AsChild: ComponentStory<typeof VisuallyHidden> = (args) => (
+  <ContrastDiv>
+    <Card>
+      <Table css={{ minHeight: 50, border: '1px dashed $hiContrast' }}>
+        <VisuallyHidden {...args}>
+          <Caption>Hidden visually</Caption>
+        </VisuallyHidden>
+      </Table>
+    </Card>
+    <Card>
+      <Table css={{ minHeight: 50, border: '1px dashed $hiContrast' }}>
+        <Caption>Not hidden visually</Caption>
+      </Table>
+    </Card>
+  </ContrastDiv>
+)
+
+AsChild.args = {
+  asChild: true,
+}
