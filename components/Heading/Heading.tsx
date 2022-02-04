dc6c2cadb0f5271a6f8771d94145fa690f0deeb8
Cedric Dupuis <cedric.dupuis@traefik.io>
2022-02-04 11:56:32 +0100

    refactor: rework Heading

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Text } from '../Text';
-import { VariantProps, CSS } from '../../stitches.config';
-import merge from 'lodash.merge';
+import { VariantProps, styled, FaencyComponentProps } from '../../stitches.config';
 
 const DEFAULT_TAG = 'h1';
 
 type TextSizeVariants = Pick<VariantProps<typeof Text>, 'size'>;
 
 type HeadingSizeVariants = '1' | '2' | '3' | '4';
-export type HeadingVariants = { size?: HeadingSizeVariants } & Omit<
+
+const StyledHeading = styled(Text, {
+  fontVariantNumeric: 'proportional-nums',
+  display: 'block',
+  lineHeight: '1.25',
+  fontWeight: '$medium',
+});
+
+export interface HeadingVariants extends Omit<
   VariantProps<typeof Text>,
   'size'
->;
-export type HeadingProps = React.ComponentProps<typeof DEFAULT_TAG> &
-  HeadingVariants & { as?: any };
+> { size?: HeadingSizeVariants }
+export interface HeadingProps extends FaencyComponentProps<typeof StyledHeading>,
+  HeadingVariants { as?: any, children?: React.ReactNode };
 
-export const Heading = React.forwardRef<React.ElementRef<typeof DEFAULT_TAG>, HeadingProps>(
+
+export const Heading = React.forwardRef<React.ElementRef<typeof StyledHeading>, HeadingProps>(
   // '2' here is the default heading size variant
   (props, forwardedRef) => {
     // '2' here is the default heading size variant
@@ -27,26 +35,12 @@ export const Heading = React.forwardRef<React.ElementRef<typeof DEFAULT_TAG>, He
       3: { '@initial': '7' },
       4: { '@initial': '10' },
     };
-
-    // This is the mapping of Heading Variants to Text css
-    const textCss: Record<HeadingSizeVariants, CSS> = {
-      1: { fontWeight: '$medium', lineHeight: '20px' },
-      2: { fontWeight: '$medium', lineHeight: '25px' },
-      3: { fontWeight: '$medium', lineHeight: '33px' },
-      4: { fontWeight: '$medium', lineHeight: '35px' },
-    };
-
     return (
-      <Text
+      <StyledHeading
         as={DEFAULT_TAG}
-        {...textProps}
         ref={forwardedRef}
         size={textSize[size]}
-        css={{
-          fontVariantNumeric: 'proportional-nums',
-          display: 'block',
-          ...(merge(textCss[size], props.css) as object),
-        }}
+        {...textProps}
       />
     );
   }
