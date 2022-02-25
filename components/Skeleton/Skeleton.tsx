3ad61217e2c0bf9a866513c36794772e23a89063
Seedy <cedric.dupuis@traefik.io>
2022-02-25 15:50:08 +0100

    feat!: skeleton infer dimensions

--- components/Skeleton/Skeleton.tsx
+++ components/Skeleton/Skeleton.tsx
@@ -10,6 +10,14 @@ export const Skeleton = styled('div', {
   position: 'relative',
   overflow: 'hidden',
   borderRadius: '3px',
+  height: 'auto',
+  width: 'auto',
+  '&:not(:empty)': {
+    '& > *': {
+      visibility: 'hidden',
+    },
+    maxWidth: 'fit-content',
+  },
 
   '&::after': {
     animationName: `${pulse}`,
@@ -29,53 +37,22 @@ export const Skeleton = styled('div', {
 
   variants: {
     variant: {
-      avatar1: {
-        borderRadius: '$round',
-        height: '$3',
-        width: '$3',
+      square: {
+        borderRadius: '$2',
       },
-      avatar2: {
-        borderRadius: '$round',
-        height: '$5',
-        width: '$5',
-      },
-      avatar3: {
-        borderRadius: '$round',
-        height: '$6',
-        width: '$6',
-      },
-      avatar4: {
-        borderRadius: '$round',
-        height: '$7',
-        width: '$7',
-      },
-      avatar5: {
-        borderRadius: '$round',
-        height: '$8',
-        width: '$8',
-      },
-      avatar6: {
-        borderRadius: '$round',
-        height: '$9',
-        width: '$9',
-      },
-      text: {
-        height: '$1',
-      },
-      title: {
-        height: '$5',
-      },
-      heading: {
-        height: '$3',
+      circle: {
+        borderRadius: '$round'
       },
       badge: {
         borderRadius: '$pill',
-        height: '$4',
       },
       button: {
-        borderRadius: '$1',
-        height: '32px',
-        width: '128px',
+        borderRadius: '$3',
+      },
+      text: {
+        '&:empty:before': {
+          content: '"\\00a0"', // adds a space character before element
+        }
       },
     },
   },
