1bece37f37fad984b45a125db96d02a5cac42b06
Cedric Dupuis <seedy.dupuis@gmail.com>
2022-04-05 17:56:32 +0200

    chore: button, remove unused theme variables

--- components/Button/Button.themes.ts
+++ components/Button/Button.themes.ts
@@ -8,14 +8,11 @@ export namespace Theme {
     buttonPrimaryText: Property.Color;
     buttonPrimaryFocusBorder: Property.Color;
     buttonPrimaryGhostHoverText: Property.Color;
-    buttonPrimaryGhostText: Property.Color;
 
     buttonSecondaryBg: Property.Color;
     buttonSecondaryText: Property.Color;
     buttonSecondaryBorder: Property.Color;
     buttonSecondaryFocusBorder: Property.Color;
-    buttonSecondaryGhostHoverText: Property.Color;
-    buttonSecondaryGhostText: Property.Color;
 
     buttonRedBg: Property.Color;
     buttonRedText: Property.Color;
@@ -30,14 +27,11 @@ export namespace Theme {
     buttonPrimaryText: 'white',
     buttonPrimaryFocusBorder: primaryColor.helpers.pickScale(6, { alpha: true }),
     buttonPrimaryGhostHoverText: '$deepBlue9',
-    buttonPrimaryGhostText: '$primary',
 
     buttonSecondaryBg: 'transparent',
     buttonSecondaryText: 'hsla(0, 0%, 0%, 0.54)',
     buttonSecondaryBorder: '$grayBlue9',
     buttonSecondaryFocusBorder: tinycolor(primaryColor.value).setAlpha(0.19).toHslString(),
-    buttonSecondaryGhostHoverText: '$deepBlue7',
-    buttonSecondaryGhostText: '$deepBlue6',
 
     buttonRedBg: '$red9',
     buttonRedText: '$loContrast',
@@ -56,8 +50,6 @@ export namespace Theme {
     buttonSecondaryText: tinycolor('white').setAlpha(0.74).toHslString(),
     buttonSecondaryBorder: '$grayBlue9',
     buttonSecondaryFocusBorder: '$primary',
-    buttonSecondaryGhostHoverText: '$deepBlue6',
-    buttonSecondaryGhostText: '$deepBlue7',
 
     buttonRedBg: '$red10',
     buttonRedText: '$hiContrast',
