275eb15c05fcb9e6d2e5da0c0a78393f32d27b5d
Seedy <seedy.dupuis@gmail.com>
2022-04-11 17:38:08 +0200

    feat!: dialog export back close primitive, export close icon button

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ export { Checkbox } from './components/Checkbox';
 export { Container } from './components/Container';
 export { Elevation, elevationVariants } from './components/Elevation';
 export { FaencyProvider } from './components/FaencyProvider';
-export { Dialog, DialogClose, DialogContent, StyledContent, DialogTrigger } from './components/Dialog';
+export { Dialog, DialogClosePrimitive, DialogContent, StyledContent, DialogCloseIconButton, DialogTrigger } from './components/Dialog';
 export {
   DropdownMenu,
   DropdownMenuTrigger,
