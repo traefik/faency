08f2f33d7e2194337ad2a06363af8d740b60335f
Cedric Dupuis <seedy.dupuis@gmail.com>
2022-04-13 16:08:33 +0200

    test: heading should be rendered with proper role

---
+++ components/Heading/Heading.test.tsx
@@ -0,0 +1,12 @@
+import { H1, H2, H3, H4, H5, H6 } from './Heading';
+import { render } from '@testing-library/react';
+
+const HEADINGS = [H1, H2, H3, H4, H5, H6];
+
+describe('Heading', () => {
+  it.each(HEADINGS)('should render heading with proper role', (HeadingComponent) => {
+    const { getByRole } = render(<HeadingComponent>Heading</HeadingComponent>);
+
+    expect(getByRole('heading')).toBeInTheDocument();
+  });
+});
