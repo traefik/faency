392402a42a66df21a371b87db73d57300bff7d44
Seedy <seedy.dupuis@gmail.com>
2022-04-26 13:34:10 +0200

    chore: doc stories, no longer rely on Heading obsolete component

--- stories/examples/dashboard.stories.tsx
+++ stories/examples/dashboard.stories.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
-import { Badge, Box, Button, Card, Flex, Heading, Text, TextField } from '../../index';
+import { Badge, Box, Button, Card, Flex, H1, H2, H3, Text, TextField } from '../../index';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 
 export const Dashboard = () => (
   <Box css={{ p: '$6' }}>
-    <Heading size="4" css={{ mb: '$3' }}>
-      Dashboard Example
-    </Heading>
+    <H1 css={{ mb: '$3' }}>Dashboard Example</H1>
     <Box as="section" css={{ mb: '$3' }}>
       <Flex css={{ justifyContent: 'space-between' }}>
-        <Heading as="h2" size="3" css={{ mb: '$3' }}>
-          Items
-        </Heading>
+        <H2 css={{ mb: '$3' }}>Items</H2>
 
         <Flex align="center" css={{ gap: '$3' }}>
           <TextField startAdornment={<MagnifyingGlassIcon />} placeholder="Search..." clearable />
@@ -22,14 +18,12 @@ export const Dashboard = () => (
       <Flex css={{ gap: '$3' }}>
         <Card css={{ flex: 1 }}>
           <Flex css={{ gap: '$3', mb: '$3' }}>
-            <Heading as="h3" size="2">
-              Item 1
-            </Heading>
+            <H3>Item 1</H3>
             <Flex css={{ gap: '$2', alignItems: 'center' }}>
-              <Badge as="button" variant="blue" interactive>
+              <Badge variant="blue" interactive>
                 Article
               </Badge>
-              <Badge as="button" variant="orange" interactive>
+              <Badge variant="orange" interactive>
                 Networking
               </Badge>
             </Flex>
@@ -45,14 +39,12 @@ export const Dashboard = () => (
         </Card>
         <Card css={{ flex: 1 }}>
           <Flex css={{ gap: '$3', mb: '$3' }}>
-            <Heading as="h3" size="2">
-              Item 2
-            </Heading>
+            <H3>Item 2</H3>
             <Flex css={{ gap: '$2', alignItems: 'center' }}>
-              <Badge as="button" variant="blue" interactive>
+              <Badge variant="blue" interactive>
                 Article
               </Badge>
-              <Badge as="button" variant="green" interactive>
+              <Badge variant="green" interactive>
                 Kubernetes
               </Badge>
             </Flex>
@@ -68,14 +60,12 @@ export const Dashboard = () => (
         </Card>
         <Card css={{ flex: 1 }}>
           <Flex css={{ gap: '$3', mb: '$3' }}>
-            <Heading as="h3" size="2">
-              Item 3
-            </Heading>
+            <H3>Item 3</H3>
             <Flex css={{ gap: '$2', alignItems: 'center' }}>
-              <Badge as="button" variant="red" interactive>
+              <Badge variant="red" interactive>
                 Video
               </Badge>
-              <Badge as="button" variant="neon" interactive>
+              <Badge variant="neon" interactive>
                 Traefik
               </Badge>
             </Flex>
