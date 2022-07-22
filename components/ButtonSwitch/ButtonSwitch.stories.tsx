6eeaae904ab4022611729eb89774086a292863c9
Paulo Junior <paulo.cezar@traefik.io>
2022-07-22 10:28:00 -0300

    feat: add story for type=multiple

--- components/ButtonSwitch/ButtonSwitch.stories.tsx
+++ components/ButtonSwitch/ButtonSwitch.stories.tsx
@@ -11,16 +11,22 @@ export const Basic: ComponentStory<any> = (args) => {
   const [value, setValue] = useState('left');
 
   return (
-    <ButtonSwitchContainer
-      type="single"
-      value={value}
-      onValueChange={(value) => {
-        if (value) setValue(value);
-      }}
-    >
+    <ButtonSwitchContainer type="single" value={value} onValueChange={setValue}>
       <ButtonSwitchItem value="left">Left</ButtonSwitchItem>
-      {/* <ButtonSwitchItem value="center">Center</ButtonSwitchItem> */}
       <ButtonSwitchItem value="right">Right</ButtonSwitchItem>
+    </ButtonSwitchContainer>
+  );
+};
+
+export const Multiple: ComponentStory<any> = (args) => {
+  const [value, setValue] = useState<string[]>([]);
+
+  return (
+    <ButtonSwitchContainer type="multiple" value={value} onValueChange={setValue}>
+      <ButtonSwitchItem value="option1">Option 1</ButtonSwitchItem>
+      <ButtonSwitchItem value="option2">Option 2</ButtonSwitchItem>
+      <ButtonSwitchItem value="option3">Option 3</ButtonSwitchItem>
+      <ButtonSwitchItem value="option4">Option 4</ButtonSwitchItem>
     </ButtonSwitchContainer>
   );
 };
