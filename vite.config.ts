ea1ca0e4fce4b0920a479582093c8300168bf4e5
Massimiliano D'Elia <massimiliano.delia@traefik.io>
2024-08-05 12:56:28 +0200

    fix: Vite error "failed to fetch dynamically imported module"

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['storybook'],
+  },
   plugins: [react()],
 });
