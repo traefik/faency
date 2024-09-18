c873f006cd1fa714c338af9119b3db6f9802d599
Massimiliano D. <126668030+mdeliatf@users.noreply.github.com>
2024-09-18 14:20:04 +0200

    feat: add blockquote

---
+++ components/Blockquote/Blockquote.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryFn } from '@storybook/react';
+import React from 'react';
+
+import { modifyVariantsForStory } from '../../utils/modifyVariantsForStory';
+import { Blockquote, BlockquoteProps, BlockquoteVariants } from './Blockquote';
+
+const BaseBlockquote = (props: BlockquoteProps): JSX.Element => <Blockquote {...props} />;
+
+const BlockquoteForStory = modifyVariantsForStory<BlockquoteVariants, BlockquoteProps>(
+  BaseBlockquote,
+);
+
+const Component: Meta<typeof BlockquoteForStory> = {
+  title: 'Components/Blockquote',
+  component: BlockquoteForStory,
+};
+
+const Template: StoryFn<typeof BlockquoteForStory> = (args) => (
+  <Blockquote {...args}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Blockquote>
+);
+
+export const Basic: StoryFn<typeof BlockquoteForStory> = Template.bind({});
+
+export default Component;
