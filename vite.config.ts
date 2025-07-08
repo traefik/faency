f66ac38784035108e8121ccac79100b0ad77ea68
gndz07 <ginadzani@gmail.com>
2025-07-08 15:14:51 +0200

    chore: build with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
 import react from '@vitejs/plugin-react';
+import { createRequire } from 'module';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['storybook'],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: 'tsconfig-rollup.json',
+      outDir: 'dist/types',
+      insertTypesEntry: true,
+      cleanVueFileName: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: './index.ts',
+      name: 'Faency',
+      formats: ['es', 'cjs'],
+      fileName: (format, entryName) => `${entryName}.${format === 'es' ? 'js' : 'cjs'}`,
+    },
+    rollupOptions: {
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        preserveModules: true,
+        preserveModulesRoot: '.',
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    minify: false,
   },
-  plugins: [react()],
 });
