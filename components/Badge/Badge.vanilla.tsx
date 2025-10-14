aab160d015acb68680aa7e41f646cf92aa0433d7
gndz07 <ginadzani@gmail.com>
2025-10-14 09:53:41 +0200

    fix: badge to be able to process css

--- components/Badge/Badge.vanilla.tsx
+++ components/Badge/Badge.vanilla.tsx
@@ -1,7 +1,10 @@
 import { Slot } from '@radix-ui/react-slot';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
 import { RecipeVariants } from '@vanilla-extract/recipes';
 import React, { useMemo } from 'react';
 
+import { CSSProps, processCSSProp } from '../../styles/cssProps';
+import { useVanillaExtractTheme } from '../../styles/themeContext';
 import { badgeRecipe, COLORS, interactiveBadgeRecipe } from './Badge.vanilla.css';
 
 export { COLORS };
@@ -9,13 +12,27 @@ export { COLORS };
 type BadgeRecipeVariants = RecipeVariants<typeof badgeRecipe>;
 
 export type BadgeVanillaProps = Omit<React.HTMLAttributes<HTMLElement>, 'color'> &
-  BadgeRecipeVariants & {
+  BadgeRecipeVariants &
+  CSSProps & {
     asChild?: boolean;
     interactive?: boolean;
   };
 
 export const BadgeVanilla = React.forwardRef<HTMLElement, BadgeVanillaProps>(
-  ({ interactive, asChild, className, size, variant, alphaBg, borderless, ...props }, ref) => {
+  (
+    { interactive, asChild, className, css, style, size, variant, alphaBg, borderless, ...props },
+    ref,
+  ) => {
+    const { colors } = useVanillaExtractTheme();
+
+    const { style: cssStyles, vars } = processCSSProp(css, colors);
+
+    const mergedStyles = {
+      ...cssStyles,
+      ...style,
+      ...assignInlineVars(vars),
+    };
+
     const recipe = interactive ? interactiveBadgeRecipe : badgeRecipe;
     const recipeClass = recipe({ size, variant, alphaBg, borderless });
 
@@ -30,6 +47,7 @@ export const BadgeVanilla = React.forwardRef<HTMLElement, BadgeVanillaProps>(
       <Component
         ref={ref as React.Ref<any>}
         className={`${recipeClass} ${className || ''}`.trim()}
+        style={mergedStyles}
         {...props}
       />
     );
