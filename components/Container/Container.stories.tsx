b30794fae3b4f2cb3887cb7c6f0b14da75165e16
gndz07 <ginadzani@gmail.com>
2025-11-18 15:50:33 +0100

    chore: migrate container

--- components/Container/Container.stories.tsx
+++ components/Container/Container.stories.tsx
@@ -5,6 +5,7 @@ import { VariantProps } from '../../stitches.config';
 import { modifyVariantsForStory } from '../../utils/modifyVariantsForStory';
 import { Paragraph } from '../Paragraph';
 import { Container } from './Container';
+import { ContainerVanilla } from './Container.vanilla';
 
 type ContainerVariants = VariantProps<typeof Container>;
 type ContainerProps = ContainerVariants & NonNullable<unknown>;
@@ -46,5 +47,32 @@ Size.args = { size: '1' };
 export const NoGutter: StoryFn<typeof ContainerForStory> = Template.bind({});
 
 NoGutter.args = { noGutter: true };
+
+export const Comparison: StoryFn = (args) => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '24px' }}>
+    <div>
+      <h3>Original Stitches Version</h3>
+      <Template {...args} />
+    </div>
+
+    <div>
+      <h3>Vanilla-Extract Version (Same API)</h3>
+      <ContainerVanilla {...args} css={{ bc: '$deepBlue3' }}>
+        <Paragraph>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, iste. Perferendis saepe
+          aperiam repudiandae, a ea labore error iure! Doloribus sunt earum, aperiam facilis ex
+          corporis veniam deleniti voluptatibus laudantium? Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Reiciendis consequatur harum quibusdam! Facilis aspernatur est fugiat
+          laudantium officiis. Aut labore asperiores qui iure earum culpa, voluptatum explicabo
+          commodi quis dolorem? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore
+          dolorum deserunt, consectetur numquam minus error dolorem? Explicabo iure quidem, maxime
+          fugit quos obcaecati, molestiae nemo nobis aliquid saepe, impedit at.
+        </Paragraph>
+      </ContainerVanilla>
+    </div>
+  </div>
+);
+
+Comparison.args = { noGutter: false, size: '1' };
 
 export default Component;
