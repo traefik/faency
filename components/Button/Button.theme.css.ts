493840b053e3eb51452f45783848fc09324cac3c
Massimiliano D'Elia <massimiliano.delia@traefik.io>
2025-12-12 14:48:04 +0100

    chore: migrate Button

---
+++ components/Button/Button.theme.css.ts
@@ -0,0 +1,3 @@
+// Re-export from plain TypeScript file to avoid circular dependencies
+// The source of truth is Button.theme.ts
+export { buttonLightTheme, buttonDarkTheme } from './Button.theme';
--- components/Button/index.ts
+++ components/Button/index.ts
@@ -1,1 +1,3 @@
 export * from './Button';
+export type { ButtonVanillaProps } from './Button.vanilla';
+export { ButtonVanilla } from './Button.vanilla';
